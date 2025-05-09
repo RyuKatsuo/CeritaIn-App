d5010e3f393dbc151521c20a0c49d27eb09ef453
AnandaAndreas <anandaandreas@students.amikom.ac.id>
2025-05-09 23:45:12 +0700

    fix skip to content

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,4 +2,4 @@ export const ACCESS_TOKEN_KEY = 'accessToken';
 
 export const BASE_URL = 'https://story-api.dicoding.dev/v1';
 
-export const MAP_SERVICE_API_KEY = 
+export const MAP_SERVICE_API_KEY = 's7D1imiLJTH5MgyzViw0';
